4cbb8efdd0b8362478246409e74c1bc0e7953050
unknown <paulocesar100limites@gmail.com>
2023-11-21 11:02:14 -0300

    Classes ok

---
+++ Biblioteca.cpp
@@ -0,0 +1,45 @@
+#include "Biblioteca.h"
+//Colocar essa condição em todo .cpp
+
+
+    Biblioteca::Biblioteca(string titulo, string autor, int ano){
+    }
+    Biblioteca::Biblioteca()
+	{
+    }
+    
+    Biblioteca::~Biblioteca(){
+    }
+
+
+
+    string Biblioteca::getTitulo(){
+        return titulo;
+    }
+    string Biblioteca::getAutor(){
+        return autor;
+    }
+    int Biblioteca::getAno(){
+        return ano;
+    }
+
+
+
+    void Biblioteca::setTitulo(string titulo){
+        this->titulo = titulo;
+    }
+
+    void Biblioteca::setAutor(string autor){
+        this->autor = autor;
+    }
+
+    void Biblioteca::setAno(int ano){
+        this->ano = ano;
+    }
+
+	void Biblioteca::imprimirBiblioteca(){
+	        cout << "Titulo: " << getTitulo() << endl;
+	        cout << "Autor: " << getAutor() << endl;
+	        cout << "Ano de publicacao: " << getAno() << endl;
+    }	
+
---
+++ Biblioteca.h
@@ -0,0 +1,41 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Biblioteca{
+
+
+    //Atributos da classe mãe
+    protected:
+        string titulo;
+        string autor;
+        int ano;
+
+
+
+    
+    public:
+
+        //construtor e destrutor
+        Biblioteca(string titulo, string autor, int ano);
+        Biblioteca();
+        ~Biblioteca();
+        
+
+        string getTitulo();
+        string getAutor();
+        int getAno();
+
+        void setTitulo(string titulo);
+        void setAutor(string autor);
+        void setAno(int ano);
+        virtual void imprimirBiblioteca();
+
+};
+
+		
+#endif
